357b0fbb3358c1831e44a9e960cb346f575d332d
trble91 <daleyaaron@gmail.com>
2023-06-07 13:37:31 -0400

    fetch request

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import React from "react";
 
 function App() {
+
+const [data, setData] = useState ({
+    artist: "",
+    song: "",
+    album: "",
+});
+
+useEffect(() => {
+  fetch(data).then((res) =>
+  res.json().then((data) => {
+    setData({
+      artist: ,
+      song: ,
+      album: ,
+    });
+  });
+  );
+}, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
   );
-}
+};
 
 export default App;
